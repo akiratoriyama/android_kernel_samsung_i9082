29900a71c634a28c62180f2655799fd4bfefa895
Shubhang <shubhang92@gmail.com>
2014-04-28 17:05:52 +0530

    dynamic filesync: add some cache optimizations

--- fs/sync.c
+++ fs/sync.c
@@ -175,10 +175,14 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 
 	#ifdef CONFIG_DYNAMIC_FSYNC
 <<<<<<< HEAD
+<<<<<<< HEAD
         if (!early_suspend_active)
         return 0;
 =======
 	if (dyn_fsync_active && !early_suspend_active)
+=======
+	if (unlikely(dyn_fsync_active && !early_suspend_active))
+>>>>>>> 4b81155... dynamic filesync: add some cache optimizations
 		return 0;
 >>>>>>> 62fec00... fs/dyn_fsync: check if dyn fsync control is active prior to performing fsync operations
 	else {
@@ -240,11 +244,15 @@ SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
 <<<<<<< HEAD
+<<<<<<< HEAD
         if (!early_suspend_active)
                  return 0;
         else
 =======
 	if (dyn_fsync_active && !early_suspend_active)
+=======
+	if (unlikely(dyn_fsync_active && !early_suspend_active))
+>>>>>>> 4b81155... dynamic filesync: add some cache optimizations
 		return 0;
 	else
 >>>>>>> 62fec00... fs/dyn_fsync: check if dyn fsync control is active prior to performing fsync operations
@@ -261,7 +269,7 @@ SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 <<<<<<< HEAD
 =======
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && !early_suspend_active)
+	if (unlikely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else
 #endif
@@ -338,11 +346,15 @@ SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
 <<<<<<< HEAD
+<<<<<<< HEAD
         if (!early_suspend_active)
                 return 0;
         else {
 =======
 	if (dyn_fsync_active && !early_suspend_active)
+=======
+	if (unlikely(dyn_fsync_active && !early_suspend_active))
+>>>>>>> 4b81155... dynamic filesync: add some cache optimizations
 		return 0;
 	else {
 >>>>>>> 62fec00... fs/dyn_fsync: check if dyn fsync control is active prior to performing fsync operations
@@ -447,11 +459,15 @@ SYSCALL_DEFINE(sync_file_range2)(int fd, unsigned int flags,
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
 <<<<<<< HEAD
+<<<<<<< HEAD
        if (!early_suspend_active)
                return 0;
        else
 =======
 	if (dyn_fsync_active && !early_suspend_active)
+=======
+	if (unlikely(dyn_fsync_active && !early_suspend_active))
+>>>>>>> 4b81155... dynamic filesync: add some cache optimizations
 		return 0;
 	else
 >>>>>>> 62fec00... fs/dyn_fsync: check if dyn fsync control is active prior to performing fsync operations
