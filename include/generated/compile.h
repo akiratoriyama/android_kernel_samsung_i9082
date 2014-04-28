577465235b3c8af029d88070aac350c5c52931ae
akiratoriyama <jaymehta.1998@gmail.com>
2014-04-28 12:23:10 +0530

    Solved some probs

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Apr 27 19:32:32 IST 2014"
+#define LINUX_COMPILE_BY "jay"
+#define LINUX_COMPILE_HOST "akiratoriyama"
+#define LINUX_COMPILER "gcc version 4.7.4 20140107 (prerelease) (crosstool-NG linaro-1.13.1+bzr2644 - Linaro GCC 2014.01 - Cortex-A9) "
